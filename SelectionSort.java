119bb47dd0a068a0fe04789efc69448aed274cad
Reppiks <30858986+Reppiks@users.noreply.github.com>
2019-04-01 01:19:15 -0500

    Update SelectionSort.java

--- SelectionSort.java
+++ SelectionSort.java
@@ -21,14 +21,18 @@ public class SelectionSort {
     public static void selectionSort(int[] arr) {
         int n = arr.length;
         int temp;
+        int minIndex;
         for (int i = 0; i < n - 1; i++ ) {
+            minIndex = i;
             for (int j = i + 1; j < n; j++) {
-                if (arr[j] < arr[i]) {
-                    temp = arr[i];
-                    arr[i] = arr[j];   
-                    arr[j] = temp;
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
                 }
-            }
+            }    
+                    temp = arr[i];
+                    arr[i] = arr[minIndex];
+                    arr[minIndex] = temp;
+                
         }
     }
 }
